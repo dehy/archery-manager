f535b46943a9967c1ef585fbe4559af13e38efa9
Arnaud de Mouhy <git@admds.net>
2022-08-27 10:35:21 +0200

    chore: updated sentry/sentry-bundle recipe

--- config/bundles.php
+++ config/bundles.php
@@ -18,6 +18,6 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
-    Sentry\SentryBundle\SentryBundle::class => ['all' => true],
+    Sentry\SentryBundle\SentryBundle::class => ['prod' => true],
     Dmishh\SettingsBundle\DmishhSettingsBundle::class => ['all' => true],
 ];
