24cabc57e5006b54f2942fccd90bf056e1f5f4f3
Arnaud de Mouhy <git@admds.net>
2022-09-16 21:44:15 +0200

    feat: new 2023 age categories

--- src/DBAL/Types/LicenseAgeCategoryType.php
+++ src/DBAL/Types/LicenseAgeCategoryType.php
@@ -11,78 +11,41 @@ use LogicException;
  */
 final class LicenseAgeCategoryType extends AbstractEnumType
 {
+    public const SENIOR_1 = "S1";
+    public const SENIOR_2 = "S2";
+    public const SENIOR_3 = "S3";
+    public const U11 = "U11";
+    public const U13 = "U13";
+    public const U15 = "U15";
+    public const U18 = "U18";
+    public const U21 = "U21";
+
+    // Old categories from previous seasons
     public const POUSSIN = "P";
     public const BENJAMIN = "B";
     public const MINIME = "M";
     public const CADET = "C";
     public const JUNIOR = "J";
-    public const SENIOR_1 = "S1";
-    public const SENIOR_2 = "S2";
-    public const SENIOR_3 = "S3";
-
-    // Old categories from previous seasons
     public const SENIOR = "S";
     public const VETERAN = "V";
     public const SUPER_VETERAN = "SV";
 
     protected static array $choices = [
+        self::U11 => "U11",
+        self::U13 => "U13",
+        self::U15 => "U15",
+        self::U18 => "U18",
+        self::U21 => "U21",
+        self::SENIOR_1 => "Senior 1",
+        self::SENIOR_2 => "Senior 2",
+        self::SENIOR_3 => "Senior 3",
         self::POUSSIN => "Poussin",
         self::BENJAMIN => "Benjamin",
         self::MINIME => "Minime",
         self::CADET => "Cadet",
         self::JUNIOR => "Junior",
-        self::SENIOR_1 => "Senior 1",
-        self::SENIOR_2 => "Senior 2",
-        self::SENIOR_3 => "Senior 3",
         self::SENIOR => "Senior",
         self::VETERAN => "Vétéran",
         self::SUPER_VETERAN => "Super Vétéran",
     ];
-
-    public static function ageCategoryFromDate(DateTimeInterface $date): string
-    {
-        if ($date >= new DateTime("01/01/2012")) {
-            return self::POUSSIN;
-        }
-        if (
-            $date >= new DateTime("01/01/2010") &&
-            $date <= new DateTime("31/12/2011")
-        ) {
-            return self::BENJAMIN;
-        }
-        if (
-            $date >= new DateTime("01/01/2008") &&
-            $date <= new DateTime("31/12/2009")
-        ) {
-            return self::MINIME;
-        }
-        if (
-            $date >= new DateTime("01/01/2005") &&
-            $date <= new DateTime("31/12/2007")
-        ) {
-            return self::CADET;
-        }
-        if (
-            $date >= new DateTime("01/01/2002") &&
-            $date <= new DateTime("31/12/2004")
-        ) {
-            return self::JUNIOR;
-        }
-        if (
-            $date >= new DateTime("01/01/1983") &&
-            $date <= new DateTime("31/12/2001")
-        ) {
-            return self::SENIOR_1;
-        }
-        if (
-            $date >= new DateTime("01/01/1963") &&
-            $date <= new DateTime("31/12/1982")
-        ) {
-            return self::SENIOR_2;
-        }
-        if ($date <= new DateTime("01/01/1963")) {
-            return self::SENIOR_3;
-        }
-        throw new LogicException("Should not be triggered");
-    }
 }
--- src/DBAL/Types/LicenseCategoryType.php
+++ src/DBAL/Types/LicenseCategoryType.php
@@ -20,21 +20,4 @@ final class LicenseCategoryType extends AbstractEnumType
         self::JEUNES => "Jeunes",
         self::ADULTES => "Adultes",
     ];
-
-    public static function categoryFromDate(DateTimeInterface $date): string
-    {
-        if ($date >= new DateTime("01/01/2012")) {
-            return self::POUSSINS;
-        }
-        if (
-            $date >= new DateTime("01/01/2002") &&
-            $date <= new DateTime("31/12/2011")
-        ) {
-            return self::JEUNES;
-        }
-        if ($date <= new DateTime("31/12/2001")) {
-            return self::ADULTES;
-        }
-        throw new LogicException("Should not be triggered");
-    }
 }
