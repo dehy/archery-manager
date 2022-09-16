feb82365c41f5d19291f91c67558330a2a7a21c8
Arnaud de Mouhy <git@admds.net>
2022-09-16 20:26:45 +0200

    chore: upgraded dependencies

--- docker/build/image_build.sh
+++ docker/build/image_build.sh
@@ -11,11 +11,11 @@ apt-get install -y --no-install-recommends ca-certificates gnupg2
 
 echo 'deb http://ppa.launchpad.net/ondrej/nginx/ubuntu jammy main' | tee /etc/apt/sources.list.d/nginx.list
 echo 'deb http://ppa.launchpad.net/ondrej/php/ubuntu jammy main' | tee /etc/apt/sources.list.d/php.list
-apt-key add /docker/build/E5267A6C.gpg
+apt-key --keyring /etc/apt/trusted.gpg.d/ondrej.gpg add /docker/build/E5267A6C.gpg
 
 echo "deb https://deb.nodesource.com/node_18.x jammy main" | tee /etc/apt/sources.list.d/nodesource.list
 echo "deb-src https://deb.nodesource.com/node_18.x jammy main" | tee -a /etc/apt/sources.list.d/nodesource.list
-apt-key add /docker/build/nodesource.gpg.key
+apt-key --keyring /etc/apt/trusted.gpg.d/nodesource.gpg add /docker/build/nodesource.gpg.key
 
 apt-get update
 apt-get upgrade -y
