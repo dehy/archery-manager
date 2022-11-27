442d47bf11f64426fd5db94b970c2039e62ca067
Arnaud de Mouhy <git@admds.net>
2022-11-27 22:11:49 +0100

    chore: php-cs-fixer

--- src/Migrations/Factory/MigrationFactoryDecorator.php
+++ src/Migrations/Factory/MigrationFactoryDecorator.php
@@ -2,11 +2,9 @@
 
 namespace App\Migrations\Factory;
 
-use App\Migrations\EntityMigrationInterface;
 use Doctrine\DBAL\Connection;
 use Doctrine\Migrations\AbstractMigration;
 use Doctrine\Migrations\Version\MigrationFactory;
-use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
 
 class MigrationFactoryDecorator implements MigrationFactory
