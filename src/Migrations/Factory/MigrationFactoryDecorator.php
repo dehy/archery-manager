757a66eb488674ded700b2a1be07b748d3cc4409
Arnaud de Mouhy <git@admds.net>
2022-11-27 22:04:28 +0100

    fix: php-cs-fixer

--- src/Migrations/Factory/MigrationFactoryDecorator.php
+++ src/Migrations/Factory/MigrationFactoryDecorator.php
@@ -14,20 +14,14 @@ class MigrationFactoryDecorator implements MigrationFactory
     public function __construct(
         private readonly Connection $connection,
         private readonly LoggerInterface $logger,
-        private readonly EntityManagerInterface $entityManager
     ) {
     }
 
     public function createVersion(string $migrationClassName): AbstractMigration
     {
-        /** @var AbstractMigration $migration */
         return new $migrationClassName(
             $this->connection,
             $this->logger
         );
-
-        /*if ($migration instanceof EntityMigrationInterface) {
-            $migration->setEntityManager($this->entityManager);
-        }*/
     }
 }
