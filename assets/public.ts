ec2da08cec1a5757091107ff92af5e129634fa91
Arnaud de Mouhy <git@admds.net>
2022-11-28 10:55:19 +0100

    chore: updated footer to include sources link

--- assets/public.ts
+++ assets/public.ts
@@ -1,8 +1,8 @@
 import "./styles/public.scss";
 
 import { library, dom } from "@fortawesome/fontawesome-svg-core";
-import { faArrowCircleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faArrowCircleLeft, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faInstagram } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faArrowCircleLeft, faFacebook, faInstagram);
+library.add(faArrowCircleLeft, faFacebook, faHeart, faInstagram);
 dom.watch();
--- assets/unauthenticated.ts
+++ assets/unauthenticated.ts
@@ -4,7 +4,8 @@ import { library, dom } from "@fortawesome/fontawesome-svg-core";
 import {
   faArrowLeft,
   faArrowRightToBracket,
+  faHeart,
 } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faArrowLeft, faArrowRightToBracket);
+library.add(faArrowLeft, faArrowRightToBracket, faHeart);
 dom.watch();
