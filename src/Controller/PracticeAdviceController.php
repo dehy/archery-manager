b7cfe73e606a961b32d394be9f1bd81bfc4aa499
Arnaud de Mouhy <git@admds.net>
2022-12-18 21:39:31 +0100

    feat: practice advice show

---
+++ src/Controller/PracticeAdviceController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\PracticeAdvice;
+use App\Entity\User;
+use App\Helper\LicenseeHelper;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PracticeAdviceController extends AbstractController
+{
+    #[Route('/practice-advices/{advice}', name: 'app_practice_advice_show')]
+    public function show(PracticeAdvice $advice, LicenseeHelper $licenseeHelper): Response
+    {
+        if ($advice->getLicensee()->getId() !== $licenseeHelper->getLicenseeFromSession()->getId()) {
+            throw $this->createNotFoundException();
+        }
+
+        return $this->render('practice_advice/show.html.twig', [
+            'advice' => $advice,
+        ]);
+    }
+}
