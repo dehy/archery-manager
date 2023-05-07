eca3afcb88f15e1b4ef0c0431fe0c83af9589dce
Arnaud de Mouhy <git@admds.net>
2023-05-07 16:50:51 +0200

    fix: removed duplicated encryption bundle

--- config/bundles.php
+++ config/bundles.php
@@ -33,5 +33,4 @@ return [
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Fidry\AliceDataFixtures\Bridge\Symfony\FidryAliceDataFixturesBundle::class => ['dev' => true, 'test' => true],
     Hautelook\AliceBundle\HautelookAliceBundle::class => ['dev' => true, 'test' => true],
-    Ambta\DoctrineEncryptBundle\AmbtaDoctrineEncryptBundle::class => ['all' => true],
 ];
