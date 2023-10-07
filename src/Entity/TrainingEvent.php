44544598e6824672df4b5d2cdaee989673488e78
Arnaud de Mouhy <git@admds.net>
2023-10-07 21:15:03 +0200

    feat: set the participation by default to registered to training event

--- src/Entity/TrainingEvent.php
+++ src/Entity/TrainingEvent.php
@@ -2,7 +2,9 @@
 
 namespace App\Entity;
 
+use App\DBAL\Types\EventParticipationStateType;
 use App\Repository\EventRepository;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EventRepository::class)]
@@ -12,4 +14,23 @@ class TrainingEvent extends Event
     #[ORM\GeneratedValue]
     #[ORM\Column(type: \Doctrine\DBAL\Types\Types::INTEGER)]
     private ?int $id = null;
+
+    public function getParticipations(): Collection
+    {
+        $participations = $this->participations;
+        foreach ($this->getAssignedGroups() as $assignedGroup) {
+            foreach ($assignedGroup->getLicensees() as $licensee) {
+                if ($this->participations->exists(fn(int $key, EventParticipation $element) => $element->getParticipant() === $licensee)) {
+                    continue;
+                }
+                $participations[] = (new EventParticipation())
+                    ->setEvent($this)
+                    ->setParticipant($licensee)
+                    ->setActivity($this->getDiscipline())
+                    ->setParticipationState(EventParticipationStateType::REGISTERED);
+            }
+        }
+
+        return $participations;
+    }
 }
