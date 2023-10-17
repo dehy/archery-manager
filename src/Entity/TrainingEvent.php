e6392583da46228bd0e90f267deb6fa729f2937f
Arnaud de Mouhy <git@admds.net>
2023-10-17 20:45:12 +0200

    Revert "feat: set the participation by default to registered to training event"
    This reverts commit 44544598e6824672df4b5d2cdaee989673488e78.
    
    # Conflicts:
    #	src/Entity/TrainingEvent.php

--- src/Entity/TrainingEvent.php
+++ src/Entity/TrainingEvent.php
@@ -2,9 +2,7 @@
 
 namespace App\Entity;
 
-use App\DBAL\Types\EventParticipationStateType;
 use App\Repository\EventRepository;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: EventRepository::class)]
@@ -14,23 +12,4 @@ class TrainingEvent extends Event
     #[ORM\GeneratedValue]
     #[ORM\Column(type: \Doctrine\DBAL\Types\Types::INTEGER)]
     private ?int $id = null;
-
-    public function getParticipations(): Collection
-    {
-        $participations = $this->participations;
-        foreach ($this->getAssignedGroups() as $assignedGroup) {
-            foreach ($assignedGroup->getLicensees() as $licensee) {
-                if ($this->participations->exists(fn (int $key, EventParticipation $element) => $element->getParticipant() === $licensee)) {
-                    continue;
-                }
-                $participations[] = (new EventParticipation())
-                    ->setEvent($this)
-                    ->setParticipant($licensee)
-                    ->setActivity($this->getDiscipline())
-                    ->setParticipationState(EventParticipationStateType::REGISTERED);
-            }
-        }
-
-        return $participations;
-    }
 }
