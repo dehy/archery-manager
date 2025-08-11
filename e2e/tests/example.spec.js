a0e60b1f0fc29a295e6e3e37df9af1e5f478b674
Arnaud de Mouhy <git@admds.net>
2025-08-11 14:48:18 +0200

    Update E2E test selectors and expectations

--- e2e/tests/example.spec.js
+++ e2e/tests/example.spec.js
@@ -8,21 +8,6 @@ test('homepage', async ({ page }) => {
 
 test('swagger', async ({ page }) => {
   await page.goto('https://localhost/docs');
-  await expect(page).toHaveTitle('Hello API Platform - API Platform');
-  await expect(page.locator('.operation-tag-content > span')).toHaveCount(5);
-});
-
-test('admin', async ({ page, browserName }) => {
-  await page.goto('https://localhost/admin');
-  await page.getByLabel('Create').click();
-  await page.getByLabel('Name').fill('foo' + browserName);
-  await page.getByLabel('Save').click();
-  await expect(page).toHaveURL(/admin#\/greetings$/);
-  await page.getByText('foo' + browserName).first().click();
-  await expect(page).toHaveURL(/show$/);
-  await page.getByLabel('Edit').first().click();
-  await page.getByLabel('Name').fill('bar' + browserName);
-  await page.getByLabel('Save').click();
-  await expect(page).toHaveURL(/admin#\/greetings$/);
-  await page.getByText('bar' + browserName).first().click();
+  await expect(page).toHaveTitle('Archery Manager - API Platform');
+  await expect(page.locator('.operation-tag-content > span')).toHaveCount(100);
 });
