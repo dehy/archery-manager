3931f1105c17aae02559083349a0a19c1de65de6
Arnaud de Mouhy <git@admds.net>
2026-02-18 22:34:38 +0100

    ✨ Add email notifications for club application lifecycle events

--- src/Helper/EmailHelper.php
+++ src/Helper/EmailHelper.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Helper;
 
 use App\Entity\Club;
+use App\Entity\ClubApplication;
 use App\Entity\Licensee;
 use App\Entity\User;
 use App\Repository\LicenseeRepository;
@@ -37,6 +38,86 @@ readonly class EmailHelper
                 'licensee' => $licensee,
                 'club' => $club,
             ]);
+
+        $this->mailer->send($email);
+    }
+
+    /**
+     * Notify the club that a new application has been submitted.
+     *
+     * @throws TransportExceptionInterface
+     */
+    public function sendClubApplicationNewEmail(ClubApplication $application): void
+    {
+        $club = $application->getClub();
+        $licensee = $application->getLicensee();
+
+        $email = new TemplatedEmail()
+            ->to($club->getContactEmail())
+            ->replyTo($licensee->getUser()->getEmail())
+            ->subject(\sprintf('%s - Nouvelle demande d\'adhésion de %s %s', $club->getName(), $licensee->getFirstname(), $licensee->getLastname()))
+            ->htmlTemplate('email/club_application_new.html.twig')
+            ->context(['application' => $application]);
+
+        $this->mailer->send($email);
+    }
+
+    /**
+     * Notify the applicant their application was validated.
+     *
+     * @throws TransportExceptionInterface
+     */
+    public function sendClubApplicationValidatedEmail(ClubApplication $application): void
+    {
+        $licensee = $application->getLicensee();
+        $club = $application->getClub();
+
+        $email = new TemplatedEmail()
+            ->to($licensee->getUser()->getEmail())
+            ->replyTo($club->getContactEmail())
+            ->subject(\sprintf('%s - Ta demande d\'adhésion a été acceptée !', $club->getName()))
+            ->htmlTemplate('email/club_application_validated.html.twig')
+            ->context(['application' => $application]);
+
+        $this->mailer->send($email);
+    }
+
+    /**
+     * Notify the applicant their application was placed on the waiting list.
+     *
+     * @throws TransportExceptionInterface
+     */
+    public function sendClubApplicationWaitingListEmail(ClubApplication $application): void
+    {
+        $licensee = $application->getLicensee();
+        $club = $application->getClub();
+
+        $email = new TemplatedEmail()
+            ->to($licensee->getUser()->getEmail())
+            ->replyTo($club->getContactEmail())
+            ->subject(\sprintf('%s - Ta demande d\'adhésion est en liste d\'attente', $club->getName()))
+            ->htmlTemplate('email/club_application_waiting_list.html.twig')
+            ->context(['application' => $application]);
+
+        $this->mailer->send($email);
+    }
+
+    /**
+     * Notify the applicant their application was rejected.
+     *
+     * @throws TransportExceptionInterface
+     */
+    public function sendClubApplicationRejectedEmail(ClubApplication $application): void
+    {
+        $licensee = $application->getLicensee();
+        $club = $application->getClub();
+
+        $email = new TemplatedEmail()
+            ->to($licensee->getUser()->getEmail())
+            ->replyTo($club->getContactEmail())
+            ->subject(\sprintf('%s - Réponse à ta demande d\'adhésion', $club->getName()))
+            ->htmlTemplate('email/club_application_rejected.html.twig')
+            ->context(['application' => $application]);
 
         $this->mailer->send($email);
     }
