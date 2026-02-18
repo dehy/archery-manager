2c15f4435c831a366e358114e92efbf3478e3501
Arnaud de Mouhy <git@admds.net>
2026-02-18 22:34:55 +0100

    ✨ Show pending club applications banner to club admins

---
+++ src/Twig/PendingClubApplicationsExtension.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Twig;
+
+use App\Entity\Licensee;
+use App\Repository\ClubApplicationRepository;
+use App\Helper\LicenseeHelper;
+use App\Helper\SeasonHelper;
+use Symfony\Bundle\SecurityBundle\Security;
+
+class PendingClubApplicationsExtension
+{
+    public function __construct(
+        private readonly Security $security,
+        private readonly LicenseeHelper $licenseeHelper,
+        private readonly SeasonHelper $seasonHelper,
+        private readonly ClubApplicationRepository $applicationRepository,
+    ) {
+    }
+
+    #[\Twig\Attribute\AsTwigFunction(name: 'pending_club_applications_count')]
+    public function getPendingClubApplicationsCount(): int
+    {
+        if (!$this->security->isGranted('ROLE_CLUB_ADMIN')) {
+            return 0;
+        }
+
+        $licensee = $this->licenseeHelper->getLicenseeFromSession();
+        if (!$licensee instanceof Licensee) {
+            return 0;
+        }
+
+        $currentSeason = $this->seasonHelper->getSelectedSeason();
+        $license = $licensee->getLicenseForSeason($currentSeason);
+        if (null === $license) {
+            return 0;
+        }
+
+        $club = $license->getClub();
+
+        return \count($this->applicationRepository->findPendingByClubAndSeason($club, $currentSeason));
+    }
+}
