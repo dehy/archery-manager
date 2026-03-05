a0135ef13fac3f278fe681a33a422a7ecdfeb885
Arnaud de Mouhy <git@admds.net>
2026-03-05 21:47:40 +0100

    🐛 Fix access control for EventManagementController to require ROLE_CLUB_ADMIN

--- src/Controller/Admin/EventManagementController.php
+++ src/Controller/Admin/EventManagementController.php
@@ -24,13 +24,12 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-#[IsGranted('ROLE_ADMIN')]
+#[IsGranted('ROLE_CLUB_ADMIN')]
 class EventManagementController extends AbstractController
 {
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
         private readonly ClubHelper $clubHelper,
-        private readonly GroupRepository $groupRepository,
     ) {
     }
 
