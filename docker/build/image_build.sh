ce6278daf35108c604ef4e07a00bebe4ddb0aad7
Arnaud de Mouhy <git@admds.net>
2026-03-28 13:06:02 +0100

    🐛 Fix Docker build by migrating NodeSource to Node 24

--- docker/build/image_build.sh
+++ docker/build/image_build.sh
@@ -7,15 +7,15 @@ set -eu
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
-apt-get install -y --no-install-recommends ca-certificates gnupg2
+apt-get install -y --no-install-recommends ca-certificates curl gnupg2
 
 echo 'deb http://ppa.launchpad.net/ondrej/nginx/ubuntu jammy main' | tee /etc/apt/sources.list.d/nginx.list
 echo 'deb http://ppa.launchpad.net/ondrej/php/ubuntu jammy main' | tee /etc/apt/sources.list.d/php.list
 apt-key --keyring /etc/apt/trusted.gpg.d/ondrej.gpg add /docker/build/E5267A6C.gpg
 
-echo "deb https://deb.nodesource.com/node_18.x jammy main" | tee /etc/apt/sources.list.d/nodesource.list
-echo "deb-src https://deb.nodesource.com/node_18.x jammy main" | tee -a /etc/apt/sources.list.d/nodesource.list
-apt-key --keyring /etc/apt/trusted.gpg.d/nodesource.gpg add /docker/build/nodesource.gpg.key
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_24.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
 
 apt-get update
 apt-get upgrade -y
